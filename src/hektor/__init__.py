659ba71691391ab1bec3c67653414537e7f2e242
semantic-release <semantic-release>
2025-09-28 11:22:55 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- src/hektor/__init__.py
+++ src/hektor/__init__.py
@@ -2,6 +2,6 @@ from .index import HektorIndex
 from .hybrid import HybridSearch
 from .api import HektorSearch
 
-__version__ = "0.1.15"
+__version__ = "1.0.0"
 
 __all__ = ["HektorIndex", "HybridSearch", "HektorSearch", "__version__"]
